e6c6c575a4f818592e105e7549c327d03a3f044c
Prince Kumar <princefriends28@gmailcom>
2023-04-08 00:20:43 +0530

    added django jet theme in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Profile
 
+
 # Register your models here.
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone_number']
+    list_display_links = ('user',)
+    list_per_page = 25
